47b678d58e43a1cc2424822c73fe971c6bfcef4f
vishalreddynaga <67163328+vishalreddynaga@users.noreply.github.com>
2020-07-15 18:56:38 -0700

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+
+
+
+            return "hellsllo";
+
+        }
+/*
+        public string Index( string name )
+        {
+
+            return  name;
+
+        }
+*/
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description pageeeee.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
